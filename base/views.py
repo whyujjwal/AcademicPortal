981ae795d7be6c12bbf1b3fb7d28ca559fd8f803
ujjwaraja09 <its.ujjwalrajhere@gmail.com>
2024-03-22 13:57:45 +0530

    models updated

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+
 
 def Home(response):
-    return HttpResponse('homepage')
+    return render(response,'base/main.html')
