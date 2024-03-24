febc5066c931f13be875daff3a4435de9a1fe42a
ujjwaraja09 <its.ujjwalrajhere@gmail.com>
2024-03-24 19:04:20 +0530

    requirements

--- AcademicPortal/settings.py
+++ AcademicPortal/settings.py
@@ -24,7 +24,7 @@ load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
