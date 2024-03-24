72613e7e674f2852a3f509f1f4db3319d7e4efef
ujjwaraja09 <its.ujjwalrajhere@gmail.com>
2024-03-24 11:48:09 +0530

    courses added

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .import views
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.conf import settings
+
 
 urlpatterns = [
     path('',views.Home,name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
- 
+    path('courses/<int:course_id>/', views.CourseDetailView.as_view(), name='course_detail'),
+    path('download/<int:announcement_id>/', views.download_file, name='download_file'),
 ]
+
--- base/views.py
+++ base/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from .models import Student, Enrollment, Course
+from .models import Student, Enrollment, Course, Announcement
 from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView
+from django.conf import settings
+from django.http import HttpResponse
 
 def Home(response):
     return render(response,"base/base.html")
@@ -25,6 +27,31 @@ def dashboard(request):
         return render(request, 'student_not_found.html')
     
 
-def course_registration(request):
-    courses = Course.objects.all()
-    return render(request, 'course_registration.html', {'courses': courses})
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'base/course.html'
+    context_object_name = 'course'
+    pk_url_kwarg = 'course_id'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        course = self.get_object()
+        context['announcements'] = Announcement.objects.filter(course=course)
+        
+        student = self.request.user.student
+        try:
+            enrollment = Enrollment.objects.get(student=self.request.user, course=course)
+            context['enrollment'] = enrollment
+        except Enrollment.DoesNotExist:
+            context['enrollment'] = None
+
+        return context
+    
+    
+def download_file(request, announcement_id):
+    announcement = get_object_or_404(Announcement, id=announcement_id)
+    file_path = announcement.files.path
+    with open(file_path, 'rb') as f:
+        response = HttpResponse(f.read(), content_type='application/force-download')
+        response['Content-Disposition'] = 'attachment; filename=%s' % announcement.files.name
+        return response
